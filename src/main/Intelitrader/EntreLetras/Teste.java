6cc2db9bdda61fdb2be3e5f663f1c381df643dc2
AurelioNM <aureliomororo300@gmail.com>
2021-11-10 10:19:05 -0300

    Entre Letras v2.0

---
+++ src/main/Intelitrader/EntreLetras/Teste.java
@@ -0,0 +1,14 @@
+package main.Intelitrader.EntreLetras;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Alfabeto alfa = new Alfabeto();
+        alfa.constroiMapa();
+        alfa.imputUser();
+        alfa.conversor();
+        alfa.resultado();
+
+    }
+}
